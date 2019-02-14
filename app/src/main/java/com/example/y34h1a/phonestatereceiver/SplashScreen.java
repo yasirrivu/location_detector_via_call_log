e398cabace1e71d280b6396d76ccf59c00cb427d
yasirrivu <36338851+yasirrivu@users.noreply.github.com>
2019-02-14 12:19:56 +0600

    Create SplashScreen.java

---
+++ app/src/main/java/com/example/y34h1a/phonestatereceiver/SplashScreen.java
@@ -0,0 +1,29 @@
+package com.example.y34h1a.phonestatereceiver;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        Thread myThread = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    sleep(3000);
+                    Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        myThread.start();
+    }
+}
